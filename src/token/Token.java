b6b75915ded8b86c991a49485d41a49ec509d75e
CrazyGuy108 <taylor@hansenfamily.us>
2017-05-13 13:13:22 -0700

    Add parser package

--- src/token/Token.java
+++ src/token/Token.java
@@ -1,4 +1,18 @@
 package token;
 
+import expr.Expr;
+import parser.Parser;
+
 public abstract class Token {
+    public Expr nud(Parser parser) {
+        return null; // placeholder
+    }
+
+    public Expr led(Parser parser, Expr left) {
+        return null; // placeholder
+    }
+
+    public int lbp() {
+        return 0; // placeholder
+    }
 }
