d2a88733692c3e03583ed6abc88ef24a0c043e67
Vihan B <contact@vihan.org>
2017-05-25 21:08:33 -0700

    Added rough kinda sorta GUI
    Signed-off-by: Vihan B <contact@vihan.org>

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,20 @@
-import tokenizer.*;
-import java.util.Stack;
-import java.util.Scanner;
-import java.util.Arrays;
-import java.util.List;
-import java.io.Console;
+import javafx.application.*;
+import javafx.scene.*;
+import javafx.stage.*;
+import javafx.fxml.*;
+import java.io.*;
 
-public class Main {
+public class Main extends Application {
     public static void main(String[] args) {
-        List<String> argv = Arrays.asList(args);
-        boolean shouldTime = argv.contains("-time");
-
-        Scanner in = new Scanner(System.in);
-        Console c;
-        if ((c = System.console()) == null) {
-            while (in.hasNextLine()) {
-                System.out.println(Main.evaluate(in.nextLine(), shouldTime));
-            }
-        } else {
-            while (true) {
-                System.out.print("tnv> ");
-                System.out.println(Main.evaluate(c.readLine(), shouldTime));
-            }
-        }
+        launch(args);
     }
 
-    private static String evaluate(String input, boolean shouldTime) {
-        long start = System.nanoTime();
-
-//         Tokenizer tok = new Tokenizer();
-//         Stack<Token> toks = tok.feed(input);
-//         if (toks == null) return null;
-//         Stack<Token> evalStack = new Parser().parse(toks);
-//         Evaluator e = new Evaluator(evalStack);
-// 
-//         long end = System.nanoTime();
-//         System.out.println("Operation took: " + ((double)(end - start) / 1000000) + "ms");
-
-//         return e.withBigDecimal();
-        return null;
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("/Calculator.fmxl"));
+        Scene scene = new Scene(root, 300, 275);
+        stage.setTitle("hello example title dont forget to replace.");
+        stage.setScene(scene);
+        stage.show();
     }
 }
